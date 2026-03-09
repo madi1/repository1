991af38e153d6e71264732e0b69b44582ca9c2b8
madi1 <madi1@users.noreply.github.com>
2026-03-10 02:33:16 +0300

    Update app.component.ts

--- angular/src/app/app.component.ts
+++ angular/src/app/app.component.ts
@@ -1,13 +1,12 @@
 import { Component } from '@angular/core';
-import { ConsultancyService } from './consultancy.service'; // Import your service
+import { ConsultancyService } from './consultancy.service';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  templateUrl: './app.component.html', // This links to your html file
-  providers: [ConsultancyService]      // This links to your service
+  templateUrl: './app.component.html',
+  providers: [ConsultancyService]
 })
 export class AppComponent {
   title = 'madi-consultancy';
 }
-Use code with caution.

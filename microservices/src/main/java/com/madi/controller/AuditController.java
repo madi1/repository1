ce9cda1f42db6723f207770617d3a6bec99644f4
madi1 <MMuqattash@gmail.com>
2026-05-02 13:58:07 +0300

    comments

--- microservices/src/main/java/com/madi/controller/AuditController.java
+++ microservices/src/main/java/com/madi/controller/AuditController.java
@@ -3,7 +3,6 @@ package com.madi.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
-import org.springframework.mail.javamail.JavaMailSenderImpl;
 import org.springframework.web.bind.annotation.*;
 import java.util.Map;
 

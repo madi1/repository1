5734d6bd63b015f8c0f510516fb3823ac801c540
madi1 <MMuqattash@gmail.com>
2026-05-03 17:22:48 +0300

    comments

--- microservices/src/main/java/com/madi/config/WebConfig.java
+++ microservices/src/main/java/com/madi/config/WebConfig.java
@@ -1,15 +1,26 @@
 package com.madi.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
-    @Override
-    public void addViewControllers(ViewControllerRegistry registry) {
-        // Redirect any path that is not an API call back to index.html
-        registry.addViewController("/{path:[^\\.]*}")
-                .setViewName("forward:/index.html");
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        // Replace with your actual Angular Azure URL
+                        // While testing locally, you can use "http://localhost:4200"
+                        .allowedOrigins("https://azurecontainerapps.io")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
+            }
+        };
     }
 }

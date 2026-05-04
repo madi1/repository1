4c2dab55f3b7b5877a919b134a33ea842865b572
madi1 <MMuqattash@gmail.com>
2026-05-04 15:08:36 +0300

    comments

--- microservices/src/main/java/com/madi/controller/ConsultancyController.java
+++ microservices/src/main/java/com/madi/controller/ConsultancyController.java
@@ -3,13 +3,11 @@ package com.madi.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import java.util.Map;
 import java.util.HashMap;
 
 @RestController
 @RequestMapping("/api/consultancy")
-@CrossOrigin(origins = "*")
 public class ConsultancyController {
 
     @GetMapping

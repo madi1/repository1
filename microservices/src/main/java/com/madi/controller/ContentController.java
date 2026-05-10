4fb7e715b8ec57347c08cc773d4ea33919dd2c88
madi1 <MMuqattash@gmail.com>
2026-05-10 20:11:34 +0300

    comments

--- microservices/src/main/java/com/madi/controller/ContentController.java
+++ microservices/src/main/java/com/madi/controller/ContentController.java
@@ -1,6 +1,5 @@
 package com.madi.controller;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
